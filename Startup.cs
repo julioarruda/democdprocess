a9ea88281c8cde146db0a411c8c2359633459519
Julio Arruda <julio.arruda@outlook.com>
2020-12-12 16:03:23 +0000

    Ajuste Teste

--- Startup.cs
+++ Startup.cs
@@ -40,7 +40,7 @@ namespace democdprocess
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+           // app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
